2c5dd8b6cd424f2860bf2cc223e24aaa31414965
Mitchell Hurley <mdhurley@pugetsound.edu>
2021-09-24 17:53:06 -0700

    got the word grabber finished

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-var sixes = dictionary.filter(word => word.length == 6); //grabs all the elements in the array that 
-var baseword = sixes[Math.floor(Math.random() * sixes.length)]
-console.log(basewordgit)
+var sixes = dictionary.filter(word => word.length == 6) //grabs all the elements in the array that 
+
+var baseword = sixes[Math.floor(Math.random() * sixes.length)] //picks a random word from list of six letter ones
+
+var lettersinroot = [...baseword] //make an array with each character in root word
+console.log(lettersinroot)
+var possible = dictionary.filter(word => word.length <= 6)  // makes the list of all my possible guesses
+
+var guesses = possible.filter(  //trying to build list of valid words
+    function(word){
+        for (let i = 0; i < word.length; i++){   //goes through all the letters in word
+            var lettersinroot = [...baseword]   //remakes the array cause the letters get spliced away
+            if (lettersinroot.includes(word.charAt(i))){
+                lettersinroot.splice(lettersinroot.indexOf(word[i]),1)
+            } else return false;
+        }
+        return true;
+    }
+
+)
