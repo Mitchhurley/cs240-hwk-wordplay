23ef1677e06799bed051e718c4f3bf1c7c82e8dd
Mitchell Hurley <mdhurley@pugetsound.edu>
2021-09-28 16:44:33 -0700

    messed with printing

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var sixes = dictionary.filter(word => word.length == 6) //grabs all the elements
 var baseword = sixes[Math.floor(Math.random() * sixes.length)] //picks a random word from list of six letter ones
 
 var lettersinroot = [...baseword] //make an array with each character in root word
-console.log(lettersinroot)
+lettersinroot = lettersinroot.sort((a, b) => 0.5 - Math.random());  //psuedoshuffle
+
 var possible = dictionary.filter(word => word.length <= 6 && word.length >= 3)  // makes the list of all my possible guesses
 
-var guesses = possible.filter(  //trying to build list of valid words
-    function(word){
+var guesses = possible.filter(  //build list of valid words
+    function(word){ //function that checks each word
         for (let i = 0; i < word.length; i++){   //goes through all the letters in word
             var lettersinroot = [...baseword]   //remakes the array cause the letters get spliced away
             if (lettersinroot.includes(word.charAt(i))){    //checks to see if the first letter of the word is a valid one
@@ -18,15 +19,24 @@ var guesses = possible.filter(  //trying to build list of valid words
     }
 
 )
+var guessed = []; //array of size = #of words
 function checkGuess(guess){
-    if (possible.indexOf(guess) != -1){ //TODO figure out how to .includes array
-        return true
+    if (possible.includes(guess)){ 
+        
+            //reveal letters
+        
     } else return false
 }
 function buildWords(){
-    //TODO DO THING
-    
+    console.clear();
+    function buildGuessed(){
+
+    }
+    function writeWord(word){
+        //write out thang to screen
+    }
 }
+function shuffle(array){}
 /* Start looping guesses
 
 
