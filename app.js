de1deb037306d8458133384b70e1e2173b7770e4
Mitchell Hurley <mdhurley@pugetsound.edu>
2021-10-03 14:09:06 -0700

    add final touches

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var sixes = dictionary.filter((word) => word.length == 6); //grabs all the elements in the array that
+var sixes = dictionary.filter((word) => word.length == 6); //grabs all the elements in the array that are the right length
 
 var baseword = sixes[Math.floor(Math.random() * sixes.length)]; //picks a random word from list of six letter ones
 
@@ -28,38 +28,37 @@ var foundWords = []; // initialize empty array to keep track of guessed variable
 for (let i = 0; i < guesses.length; i++) { //sets all guesses to false
   foundWords[i] = false;
 }
-shuffle(lettersinroot)
+shuffle(lettersinroot) //initial shuffle so the baseword inst obvious
 printWords();
-let done = false
-do{
-   var input = prompt("Gimme guess");
-   if (input == "*") {shuffle(lettersinroot);printWords();}
+let done = false //sentinel for null input
+do{ //main guess loop
+   var input = prompt("Enter your guess");
+   if (input == "*") //case for the shuffle
+    {shuffle(lettersinroot);printWords();}
    else if (input == null) done = true;
-   else{checkGuess(input)}
+   else{checkGuess(input)} //non null or asterisk input
    
 }while (!done && !foundWords.every(Boolean)) //null input or all words being guessed ends the loop   
 //congratulate user if they got all the words
 console.clear();
 if (foundWords.every(Boolean)) console.log(`Wow! You found every word you can make from ${baseword}! Thats ${foundWords.length} different words!`)
-//TODO reveal answers and print out guessed words
-let numbo = countguessed(foundWords)
 for (let i = 0; i < guesses.length; i++) { //prints the words
     console.log(guesses[i]);
   }
-
-console.log(`Of ${foundWords.length} possible words, you guessed ${numbo} of them.`)
+console.log(`Of ${foundWords.length} possible words, you guessed ${countguessed(foundWords)} of them.`)
 
 
 
 
 //method that takes a guess and checks it, then changes the corresponding bool and reprints words
 function checkGuess(guess) {
-  if (guesses.includes(guess) && !foundWords[guesses.indexOf(guess)]) {
-    foundWords[guesses.indexOf(guess)] = true;
+  if (guesses.includes(guess) && !foundWords[guesses.indexOf(guess)]) { //checks that the word is both in the list and its corresponding boolean isnt flipped
+    foundWords[guesses.indexOf(guess)] = true; // sets the corresponding boolean of the word to true
     printWords();
     alert(`Correct! ${guess} is in the list! Can you think of any others?`);
-    //TODO alert user based on input
-  } else if (guess.length > 6) alert(`You can't spell ${guess} with the letter I gave you... but nice try!`)
+    
+  } //cases for bad input
+  else if (guess.length > 6) alert(`You can't spell ${guess} with the letter I gave you... but nice try!`)
   else if (guess.length < 3) alert(`At least use three letters, come on... ${guess} is a little too easy`)
   else if (foundWords[guesses.indexOf(guess)]) alert("You already got that one")
   
@@ -76,7 +75,7 @@ function printWords() {
     //goes through entire array of words
     if (foundWords[i] == true) {
       console.log(guesses[i].toString());
-    } //guessed word case
+    } //guessed word case prints out string normally
     else {
       let str = "";
       for (let j = 0; j < guesses[i].length; j++) {
@@ -87,6 +86,8 @@ function printWords() {
     }
   }
 }
+
+// method that shuffles the array in
 function shuffle(array) {
   let empty = []; //initialize empty auxillary array
   array.forEach((element) => {
